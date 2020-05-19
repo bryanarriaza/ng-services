ff2b9c86341a25341047f7e76b407d69c6402605
barriaza <barriaza@is4tech.com>
2020-05-19 14:42:41 -0600

    Initial commit

---
+++ src/test/java/com/is4tech/ngcourse/ngservices/NgServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is4tech.ngcourse.ngservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
